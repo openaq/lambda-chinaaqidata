4584fecb962ba43a61a95fcad24df8974b7ea67b
jflasher <joseph.flasher@gmail.com>
2018-02-20 13:17:02 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# lambda-chinaaqidata
+
+This is a simple wrapper to run https://github.com/openaq/ChinaAQIData via AWS Lambda. It'll output its data to a private S3 Bucket to be picked up by an https://github.com/openaq/openaq-fetch adapter.
--- functions/fetch/main.py
+++ functions/fetch/main.py
@@ -1,21 +1,45 @@
 from subprocess import call
 import boto3
+import requests
+from subprocess import Popen, PIPE, STDOUT
+import io
 
 
-def handler(event, context):
+def handle(event, context):
     # Download reference data from source
-    # call(["wget", "http://106.37.208.233:20035/emcpublish/ClientBin/Env-CnemcPublish-RiaServices-EnvCnemcPublishDomainService.svc/binary/GetAQIDataPublishLives", "-O", "GetAQIDataPublishLives"])
+    print("Downloading Chinese data")
+    resp = requests.get("http://106.37.208.233:20035/emcpublish/ClientBin/Env-CnemcPublish-RiaServices-EnvCnemcPublishDomainService.svc/binary/GetAQIDataPublishLives")
+    print("Data downloaded, saving locally")
+    d = open("/tmp/GetAQIDataPublishLives", "wb")
+    d.write(resp.content)
+    d.close()
 
     # Run wcf2xml and send to data.xml
-    f = open("data.xml", "w")
-    call(["python", "python-wcfbin/wcf2xml.py", "GetAQIDataPublishLives"],
-         stdout=f)
+    print("Converting data to XML")
+    f = open("/tmp/data.xml", "w")
+    cmd = "python ChinaAQIData/python-wcfbin/wcf2xml.py /tmp/GetAQIDataPublishLives"
+    p = Popen(cmd, shell=True, stdout=PIPE)
+    output = p.stdout.read()
+    f.write(output.decode("utf-8"))
+    f.close()
 
     # Convert data.xml to JSON and store in airnow.json
-    call(["python", "xml2json.py"])
+    print("Converting data to JSON")
+    cmd = "python ChinaAQIData/xml2json.py /tmp/"
+    p = Popen(cmd, shell=True, stdout=PIPE)
+    p.wait()
+    # output = p.stdout.read()
+    # print(output)
+
+    # f = open("/tmp/airnow.json", "r")
+    # data = f.read()
+    # print(data)
+    # f.close()
 
     # Save output file to S3
-    data = open("/tmp/airnows.json", "rb")
+    print("Saving data to S3")
+    f = io.open("/tmp/airnow.json", "r", encoding="utf8")
+    data = f.read()
     s3 = boto3.client('s3')
     s3.put_object(
         Body=data,
@@ -23,6 +47,7 @@ def handler(event, context):
         Bucket="openaq-chinaaqidata",
         Key="airnow.json"
     )
+    f.close()
 
     return {
         'done': True
